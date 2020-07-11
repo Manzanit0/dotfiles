1408f5c9d54221f16fd4bf5621579c946c15dca0
Manzanit0 <unico.javier@gmail.com>
2020-07-11 14:52:08 +0100

    Install docker from single script

--- ubuntu/setup.sh
+++ ubuntu/setup.sh
@@ -50,13 +50,8 @@ apt-get update
 apt-get install postgresql postgresql-contrib
 
 # Docker
-apt update
-apt install apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
-apt update
-apt-cache policy docker-ce
-apt install docker-ce
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
 
 ## Add user to docker group in order to not have to use to use docker
 usermod -a -G docker ${USER}
@@ -64,7 +59,6 @@ su - ${USER}
 
 ## Docker Compose
 curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-
 chmod +x /usr/local/bin/docker-compose
 
 # Kubernetes
@@ -82,3 +76,7 @@ rm get_helm.sh
 
 # AWS CLI
 pip install awscli
+
+# Alacritty
+add-apt-repository ppa:mmstick76/alacritty
+apt install alacritty
