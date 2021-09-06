5f0062d293367449054aad6dc5fb398cd8ef8806
Javier García <unico.javier@gmail.com>
2021-09-06 17:26:17 +0200

    Update tmux.sh

--- general/tmux.sh
+++ general/tmux.sh
@@ -9,6 +9,13 @@ if [[ "$(uname)" == "Linux" ]]; then
     apt install fonts-powerline
 fi
 
-# TODO can't remember for MacOS. We'll see next time :)
+# On MacOS you have to config iterm to use the font
+if [[ "$(uname)" == "Darwin" ]]; then
+    git clone https://github.com/powerline/fonts.git --depth=1
+    cd fonts
+    ./install.sh
+    cd ..
+    rm -rf fonts
+fi
 
 # TODO Remember to then install all tmux plugins it with ctrl + b + I
