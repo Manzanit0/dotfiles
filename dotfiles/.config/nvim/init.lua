7f50595b4ff2965eadcdac7d2bcd54b0a0b36602
Javier García <unico.javier@gmail.com>
2023-02-26 09:41:01 +0100

    Git linker

--- dotfiles/.config/nvim/init.lua
+++ dotfiles/.config/nvim/init.lua
@@ -362,6 +362,34 @@ require("packer").startup(function(use)
     end
   })
 
+  -- links to Git hosting service
+  use({ 'ruifm/gitlinker.nvim',
+    requires = 'nvim-lua/plenary.nvim',
+    config = function()
+      local gitlinker = require("gitlinker")
+      gitlinker.setup({ mappings = nil })
+
+      vim.keymap.set("n", "<leader>gb", "", {
+        silent = true,
+        desc = "browse repo in browser",
+        callback = function()
+          gitlinker.get_repo_url({
+            action_callback = gitlinker.actions.open_in_browser
+          })
+        end
+      })
+
+      vim.keymap.set({ "n", "v" }, "<leader>gl", "", {
+        silent = true,
+        desc = "get git permlink",
+        callback = function()
+          local mode = string.lower(vim.fn.mode())
+          gitlinker.get_buf_range_url(mode)
+        end,
+      })
+    end
+  })
+
   -- ui for nvim-lsp progress
   use({ "j-hui/fidget.nvim",
     config = function()
