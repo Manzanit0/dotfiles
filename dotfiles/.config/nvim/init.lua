0ff8ff81789b5b28c7fe6ae860980a5c070d8020
Javier García <unico.javier@gmail.com>
2023-03-23 11:44:28 +0100

    Hide .git directory from neotree

--- dotfiles/.config/nvim/init.lua
+++ dotfiles/.config/nvim/init.lua
@@ -257,6 +257,9 @@ require("packer").startup(function(use)
             visible = true,
             hide_dotfiles = false,
             hide_gitignored = false,
+            hide_by_name = {
+              ".git",
+            },
           },
           window = {
             mappings = {
